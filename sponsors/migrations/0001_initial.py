c9a6f9b4ffd4b6e5073861b538c70e2f80cc1d9b
ManjuGitHub8201 <82563663+ManjuGitHub8201@users.noreply.github.com>
2021-08-12 13:40:33 +0530

    updated

--- sponsors/migrations/0001_initial.py
+++ sponsors/migrations/0001_initial.py
@@ -8,6 +8,15 @@ from django.conf import settings
 
 
 class Migration(migrations.Migration):
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import markupfield.fields
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
